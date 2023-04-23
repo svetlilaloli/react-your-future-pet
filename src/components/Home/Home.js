058d3471689985644f58d42b9d5e7732552c4a67
svetlilaloli <svetlilaloli@gmail.com>
2023-04-23 08:45:11 +0100

    styles module added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,19 @@
+import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
 
 export function Home (){
     return (
-        <header className="masthead">
-            <div className="container px-4 px-lg-5 h-100">
-                <div className="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-                    <div className="col-lg-8 align-self-end">
-                        <h1 className="text-white font-weight-bold">Your Favorite Place for Adorable Buddies</h1>
-                        <hr className="divider" />
+        <header className={styles.masthead}>
+            <div className={styles.container}>
+                <div className={styles.row}>
+                    <div className={styles.colHeading}>
+                        <h1 className={styles.h1}>Your Favorite Place for Adorable Buddies</h1>
+                        <hr className={styles.divider} />
                     </div>
-                    <div className="col-lg-8 align-self-baseline">
-                        <p className="text-white-75 mb-5">Shelters are full!</p>
-                        <p className="text-white-75 mb-5">Help pets find a new home!</p>
-                        <Link className="btn btn-primary btn-xl" to="/about">Find Out More</Link>
+                    <div className={styles.colParagraph}>
+                        <p className={styles.text}>Shelters are full!</p>
+                        <p className={styles.text}>Help pets find a new home!</p>
+                        <Link className={styles.btn} to="/about">Find Out More</Link>
                     </div>
                 </div>
             </div>
